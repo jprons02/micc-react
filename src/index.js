3c8db9c32fabbfd3532dfb7ab2c527174c349787
Joseph Ronselli <joseph.ronselli@gmail.com>
2020-11-13 11:15:13 -0500

    changed folder names views that are demos

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import { Router, Route, Switch } from "react-router-dom";
 import "assets/scss/material-kit-react.scss?v=1.9.0";
 
 // pages for this product
-import Components from "views/Components/Components.js";
-import LandingPage from "views/LandingPage/LandingPage.js";
-import ProfilePage from "views/ProfilePage/ProfilePage.js";
-import LoginPage from "views/LoginPage/LoginPage.js";
+import Components from "views/_demo_Components/Components.js";
+import LandingPage from "views/_demo_LandingPage/LandingPage.js";
+import ProfilePage from "views/_demo_ProfilePage/ProfilePage.js";
+import LoginPage from "views/_demo_LoginPage/LoginPage.js";
 
 var hist = createBrowserHistory();
 
