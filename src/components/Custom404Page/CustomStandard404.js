fa1bb4cd6e8f810f9786d5e92c9c017d5f11f788
Joseph Ronselli <joseph.ronselli@yahoo.com>
2021-06-10 14:43:36 -0400

    added roxy to 404

--- src/components/Custom404Page/CustomStandard404.js
+++ src/components/Custom404Page/CustomStandard404.js
@@ -3,17 +3,37 @@ import React from "react";
 // @material-ui/core components
 import Divider from "@material-ui/core/Divider";
 
+// images
+import Roxy from "assets/img/404/Chachi_404.png";
+
 // My Custom Components
 import Button from "@material-ui/core/Button";
+
+// core components
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
 
 const CustomStandard404Page = (props) => {
   return (
     <div>
-      <h2>Page not found :(</h2>
-      <h5>
-        Maybe the page you are looking for has been removed, or you typed in the
-        wrong URL
-      </h5>
+      <div>
+        <div
+          style={{
+            display: "inline-block",
+            marginRight: "20px",
+            verticalAlign: "middle",
+          }}
+        >
+          <img style={{ width: "200px" }} src={Roxy} />
+        </div>
+        <div style={{ display: "inline-block", verticalAlign: "middle" }}>
+          <h2 style={{ marginTop: "5px" }}>Page not found :(</h2>
+          <h5>
+            Maybe the page you are looking for has been removed, or you typed in
+            the wrong URL
+          </h5>
+        </div>
+      </div>
       <Divider style={{ margin: "20px 0px 20px 0px" }} />
       <Button onClick={props.handleClick} simple="true">
         GO TO HOME PAGE
