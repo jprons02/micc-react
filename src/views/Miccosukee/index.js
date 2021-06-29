dfa5b3fc99d075725a4fa3246e7a1823922d8a1b
Joseph Ronselli <joseph.ronselli@yahoo.com>
2021-06-29 14:14:46 -0400

    fixed subdomain code for cloudfront testing

--- src/views/Miccosukee/index.js
+++ src/views/Miccosukee/index.js
@@ -46,14 +46,36 @@ const Miccosukee = () => {
     let subdomain = parts[0];
     // If we get more than 3 parts, then we have a subdomain
     // INFO: This could be 4, if you have a co.uk TLD or something like that.
-    if (parts.length >= 3) {
-      subdomain = parts[0];
-      // Remove the subdomain from the parts list
-      parts.splice(0, 1);
-      // Set the location to the new url
-      window.location =
-        protocol + "//" + parts.join(".") + "/" + subdomain + location.pathname;
+    if (host === "d3hg4fe289wfe1.cloudfront.net") {
+      if (parts.length >= 4) {
+        subdomain = parts[0];
+        // Remove the subdomain from the parts list
+        parts.splice(0, 1);
+        // Set the location to the new url
+        window.location =
+          protocol +
+          "//" +
+          parts.join(".") +
+          "/" +
+          subdomain +
+          location.pathname;
+      }
+    } else {
+      if (parts.length >= 3) {
+        subdomain = parts[0];
+        // Remove the subdomain from the parts list
+        parts.splice(0, 1);
+        // Set the location to the new url
+        window.location =
+          protocol +
+          "//" +
+          parts.join(".") +
+          "/" +
+          subdomain +
+          location.pathname;
+      }
     }
+
     console.log("location pathname: ", location.pathname);
   }, []);
 
