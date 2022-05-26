584d1e87fd30c202b94fd82bc9fa8c3390bb222d
Joseph Ronselli <joseph.ronselli@yahoo.com>
2022-05-26 16:37:45 -0400

    updated village and airboats phone number

--- src/business_info/genericInfo.js
+++ src/business_info/genericInfo.js
@@ -1,31 +1,33 @@
 // phone numbers, addressess etc...
 
 export const mrgBusinessInfo = {
-  name: "mrg",
-  phone: "305-222-4600",
-  email: "",
-  address: "500 SW 177th Ave, Miami, FL 33194",
+  name: 'mrg',
+  phone: '305-222-4600',
+  email: '',
+  address: '500 SW 177th Ave, Miami, FL 33194',
   careersLink:
-    "https://workforcenow.adp.com/mascsr/default/mdf/recruitment/recruitment.html?cid=346f5352-8215-418a-95b5-a67f6bba88ac&ccId=19000101_000001&type=MP&lang=en_US",
+    'https://workforcenow.adp.com/mascsr/default/mdf/recruitment/recruitment.html?cid=346f5352-8215-418a-95b5-a67f6bba88ac&ccId=19000101_000001&type=MP&lang=en_US',
 };
 
 export const golfBusinessInfo = {
-  name: "golf",
-  phone: "305-382-3930",
-  email: "",
-  address: "6401 Kendale Lakes Dr, Miami, FL 33183",
+  name: 'golf',
+  phone: '305-382-3930',
+  email: '',
+  address: '6401 Kendale Lakes Dr, Miami, FL 33183',
 };
 
 export const villageBusinessInfo = {
-  name: "village",
-  phone: "786-510-5896",
-  email: "",
-  address: "Mile Marker 36, US-41, Miami, FL 33194",
+  name: 'village',
+  phone: '305-222-4600',
+  //phone: "786-510-5896",
+  email: '',
+  address: 'Mile Marker 36, US-41, Miami, FL 33194',
 };
 
 export const airboatsBusinessInfo = {
-  name: "airboats",
-  phone: "786-510-5896",
-  email: "",
-  address: "Mile Marker 36, US-41, Miami, FL 33194",
+  name: 'airboats',
+  phone: '305-222-4600',
+  //phone: "786-510-5896",
+  email: '',
+  address: 'Mile Marker 36, US-41, Miami, FL 33194',
 };
--- src/views/MRG/Pages/gaming/gamingPromos/cardContent/months/5_june.js
+++ src/views/MRG/Pages/gaming/gamingPromos/cardContent/months/5_june.js
@@ -1,13 +1,25 @@
 // Images
-//import cardImage1 from "assets/media/img/mrg/monthly_promos/NewMemberBonus_Logo-768x768.png";
+import cardImage1 from 'assets/media/img/mrg/monthly_promos/0_recurring/NewMembersBonus_Logo.png';
+import cardImage2 from 'assets/media/img/mrg/monthly_promos/0_recurring/MiccosukeeOneStatusMatch.png';
+import cardImage3 from 'assets/media/img/mrg/monthly_promos/0_recurring/VIPGiveawayBlackCard_Logo.png';
+import cardImage4 from 'assets/media/img/mrg/monthly_promos/0_recurring/RedCardVIPRicochet_Logo.png';
+import cardImage5 from 'assets/media/img/mrg/monthly_promos/6_june/SizzlingSlotsFreePlay_Logo.png';
+import cardImage6 from 'assets/media/img/mrg/monthly_promos/0_recurring/GiftGiveaway_Logo.png';
+import cardImage7 from 'assets/media/img/mrg/monthly_promos/0_recurring/LunchBreakHotSeat_Logo.png';
+import cardImage8 from 'assets/media/img/mrg/monthly_promos/6_june/PVPPlinko_Logo.png';
+import cardImage9 from 'assets/media/img/mrg/monthly_promos/0_recurring/VIPGiveawayWhiteCard_Logo.png';
+import cardImage10 from 'assets/media/img/mrg/monthly_promos/6_june/FathersDayFreePlay_Logo.png';
+import cardImage11 from 'assets/media/img/mrg/monthly_promos/6_june/21Gameshow_Logo.png';
+import cardImage12 from 'assets/media/img/mrg/monthly_promos/0_recurring/BirthdayBonanzaHotSeats_Logo.png';
+import cardImage13 from 'assets/media/img/mrg/monthly_promos/0_recurring/NewMembers_ReferralsWinBig_Logo.png';
 
 export const june = {
-  month: "june",
-  monthEs: "junio",
+  month: 'june',
+  monthEs: 'junio',
   calendarLink:
-    "https://mapa-media.s3.amazonaws.com/mrg_monthly_promos/june_en.pdf",
+    'https://mapa-media.s3.amazonaws.com/mrg_monthly_promos/june_en.pdf',
   calendarLinkEs:
-    "https://mapa-media.s3.amazonaws.com/mrg_monthly_promos/june_sp.pdf",
+    'https://mapa-media.s3.amazonaws.com/mrg_monthly_promos/june_sp.pdf',
   data: undefined,
   dataEs: undefined,
 };
