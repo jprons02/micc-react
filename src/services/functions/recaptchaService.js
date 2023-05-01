a669117cfc88d8ea6097f8fa71aafadec8ab7656
Joseph Ronselli <joseph.ronselli@yahoo.com>
2023-05-01 13:57:49 -0400

    recaptcha service updated to include key

--- src/services/functions/recaptchaService.js
+++ src/services/functions/recaptchaService.js
@@ -1,27 +1,28 @@
-import axios from "axios";
-import { keys } from "keys.js";
+import axios from 'axios';
+import { keys } from 'keys.js';
 
 export const reCaptchaService = async (clientResponse, callback) => {
   const dataString = JSON.stringify({
-    clientResponse,
+    clientResponse: clientResponse,
+    secretKey: keys.recaptchaSecret,
   });
 
   try {
     const response = await axios({
-      method: "POST",
-      url: "https://2hl09omy22.execute-api.us-east-1.amazonaws.com/prod",
+      method: 'POST',
+      url: 'https://2hl09omy22.execute-api.us-east-1.amazonaws.com/prod',
       data: dataString,
       headers: {
-        "Content-Type": "application/json",
-        "x-api-key": keys.lambdaReCaptchaAPI,
+        'Content-Type': 'application/json',
+        'x-api-key': keys.lambdaReCaptchaAPI,
       },
     });
     if (response.data.success != true) {
-      callback("fail");
+      callback('fail');
     } else {
-      callback("success");
+      callback('success');
     }
   } catch (error) {
-    console.log("error: ", error);
+    console.log('error: ', error);
   }
 };
