baf7bfdc563f0af797de766d4f5e4e5787b1efc6
Joseph Ronselli <joseph.ronselli@yahoo.com>
2023-06-05 07:38:53 -0600

    updated calendar link

--- src/views/MRG/Pages/gaming/gamingPromos/machinePromos/cardContent/months/5_june.js
+++ src/views/MRG/Pages/gaming/gamingPromos/machinePromos/cardContent/months/5_june.js
@@ -12,9 +12,9 @@ export const june = {
   month: 'june',
   monthEs: 'junio',
   calendarLink:
-    'https://mapa-media.s3.amazonaws.com/mrg_monthly_promos/july_en.pdf',
+    'https://mapa-media.s3.amazonaws.com/mcr_monthly_promos/june_en.pdf',
   calendarLinkEs:
-    'https://mapa-media.s3.amazonaws.com/mrg_monthly_promos/july_sp.pdf',
+    'https://mapa-media.s3.amazonaws.com/mcr_monthly_promos/june_sp.pdf',
   data: [
     {
       id: 0,
