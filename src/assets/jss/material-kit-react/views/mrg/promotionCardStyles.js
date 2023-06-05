840d35dac20010816e6c164aad8d3c67062ca0f0
Joseph Ronselli <joseph.ronselli@yahoo.com>
2023-06-05 10:11:48 -0600

    updated promo card design height

--- src/assets/jss/material-kit-react/views/mrg/promotionCardStyles.js
+++ src/assets/jss/material-kit-react/views/mrg/promotionCardStyles.js
@@ -1,58 +1,58 @@
-import { mrgColor } from "themes/colors.js";
+import { mrgColor } from 'themes/colors.js';
 
 const promoCardStyle = {
   cardContainer: {
-    marginLeft: "auto",
-    marginRight: "auto",
-    textAlign: "center",
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    textAlign: 'center',
   },
   serviceCards: {
-    display: "block",
-    width: "95%",
-    marginLeft: "auto",
-    marginRight: "auto",
-    textAlign: "left",
-    border: "1px solid #969696",
-    boxShadow: "none",
+    display: 'block',
+    width: '95%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    textAlign: 'left',
+    border: '1px solid #969696',
+    boxShadow: 'none',
   },
   imgStyle: {
-    margin: "auto",
-    width: "200px",
-    display: "block",
-    paddingTop: "15px",
+    margin: 'auto',
+    width: '200px',
+    display: 'block',
+    paddingTop: '15px',
   },
   cardTitle: {
-    fontFamily: "sans-serif",
-    fontSize: "14px",
-    textAlign: "center",
-    minHeight: "40px",
+    fontFamily: 'sans-serif',
+    fontSize: '14px',
+    textAlign: 'center',
+    minHeight: '130px',
   },
   cardBodyText: {
-    fontSize: "10px",
+    fontSize: '10px',
   },
   buttonStyle: {
-    display: "block",
-    margin: "auto",
-    marginBottom: "10px",
+    display: 'block',
+    margin: 'auto',
+    marginBottom: '10px',
     //backgroundColor: "transparent !important",
     //border: `1px solid ${mrgColor[500]}`,
     //color: mrgColor[500],
-    fontWeight: "500",
+    fontWeight: '500',
   },
   dots: {
-    position: "relative !important",
-    marginTop: "-5px",
+    position: 'relative !important',
+    marginTop: '-5px',
   },
   //DESKTOP VIEW
-  "@media screen and (min-width: 960px)": {
+  '@media screen and (min-width: 960px)': {
     cardContainer: {
-      textAlign: "center",
-      paddingBottom: "15px",
+      textAlign: 'center',
+      paddingBottom: '15px',
     },
     serviceCards: {
-      display: "inline-block",
-      margin: "24px",
-      width: "20rem",
+      display: 'inline-block',
+      margin: '24px',
+      width: '20rem',
     },
   },
 };
