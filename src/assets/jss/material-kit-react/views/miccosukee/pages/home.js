c038e72a04a3d71e97635310b1bbc8a0e3c11e8a
Joseph Ronselli <joseph.ronselli@yahoo.com>
2023-06-28 20:36:42 -0400

    updated desktop view height of home page

--- src/assets/jss/material-kit-react/views/miccosukee/pages/home.js
+++ src/assets/jss/material-kit-react/views/miccosukee/pages/home.js
@@ -1,51 +1,55 @@
-import { container } from "assets/jss/material-kit-react.js";
-import { cardTitle } from "assets/jss/material-kit-react.js";
+import { container } from 'assets/jss/material-kit-react.js';
+import { cardTitle } from 'assets/jss/material-kit-react.js';
 
 const miccosukeeHomePageStyle = {
   container,
   gridContainer: {
-    height: "100vh",
-    maxHeight: "900px",
-    paddingBottom: "30px",
-    position: "absolute",
-    width: "100%",
-    margin: "auto",
+    height: '100vh',
+    maxHeight: '900px',
+    paddingBottom: '30px',
+    position: 'absolute',
+    width: '100%',
+    margin: 'auto',
+    //DESKTOP VIEW
+    '@media screen and (min-width: 960px)': {
+      height: '400px',
+    },
   },
   gridItem: {
-    padding: "15px 0px",
-    height: "50%",
-    minHeight: "350px",
-    width: "100%",
+    padding: '15px 0px',
+    height: '50%',
+    minHeight: '350px',
+    width: '100%',
   },
   cardTitle,
   mCard: {
-    width: "100%",
-    height: "100%",
-    margin: "0",
-    cursor: "pointer",
+    width: '100%',
+    height: '100%',
+    margin: '0',
+    cursor: 'pointer',
   },
   mCardHeader: {
-    padding: "0 1.875rem",
-    backgroundColor: "#0000008a",
-    color: "white",
-    borderTopLeftRadius: "6px",
-    borderTopRightRadius: "6px",
+    padding: '0 1.875rem',
+    backgroundColor: '#0000008a',
+    color: 'white',
+    borderTopLeftRadius: '6px',
+    borderTopRightRadius: '6px',
   },
   mCardDescription: {
-    marginTop: "-8px",
+    marginTop: '-8px',
   },
   main: {
-    zIndex: "3",
-    maxWidth: "1200px",
-    margin: "auto",
+    zIndex: '3',
+    maxWidth: '1200px',
+    margin: 'auto',
   },
   //DESKTOP VIEW
-  "@media screen and (min-width: 960px)": {
+  '@media screen and (min-width: 960px)': {
     gridItem: {
-      padding: "15px",
-      height: "50%",
-      minHeight: "350px",
-      width: "100%",
+      padding: '15px',
+      height: '50%',
+      minHeight: '350px',
+      width: '100%',
     },
   },
 };
